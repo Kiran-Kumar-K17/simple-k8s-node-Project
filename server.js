451c26c5f26c998fd80163624a0637db97cfcf1f
Kiran Kumar <158074959+Kiran-Kumar-K17@users.noreply.github.com>
2026-01-03 15:26:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Welcome to K8s");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on PORT: ${PORT}`);
+});
